38646fac0b2a7f58e9091d573a433af1968f1152
WNAOII <wnaoii@163.com>
2021-11-27 20:30:03 +0800

    first commit

--- springboot/src/main/java/com/wnaoii/demo/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.wnaoii.demo.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.wnaoii.demo.entity.User;
-
-public interface UserMapper extends BaseMapper<User> {
-}
